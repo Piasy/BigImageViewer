145c4617167f84851df7e50b370d89a402a9b668
Piasy <xz4215@gmail.com>
2017-03-20 19:57:01 +0800

    optimizeDisplay also control the animation when show big image and hide thumbnail.

--- BigImageViewer/src/main/java/com/github/piasy/biv/view/BigImageView.java
+++ BigImageViewer/src/main/java/com/github/piasy/biv/view/BigImageView.java
@@ -283,44 +283,61 @@ public class BigImageView extends FrameLayout implements ImageLoader.Callback {
         post(new Runnable() {
             @Override
             public void run() {
-                AnimationSet set = new AnimationSet(true);
-                AlphaAnimation animation = new AlphaAnimation(1, 0);
-                animation.setDuration(500);
-                animation.setFillAfter(true);
-                set.addAnimation(animation);
-                if (mThumbnailView != null) {
-                    mThumbnailView.setAnimation(set);
-                }
-
-                if (mProgressIndicator != null) {
-                    mProgressIndicator.onFinish();
-                }
-
-                if (mProgressIndicatorView != null) {
-                    mProgressIndicatorView.setAnimation(set);
-                }
-
-                animation.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        if (mThumbnailView != null) {
-                            mThumbnailView.setVisibility(GONE);
-                        }
-                        if (mProgressIndicatorView != null) {
-                            mProgressIndicatorView.setVisibility(GONE);
-                        }
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-                    }
-                });
+                doOnFinish();
             }
         });
+    }
+
+    @UiThread
+    private void doOnFinish() {
+        if (mOptimizeDisplay) {
+            AnimationSet set = new AnimationSet(true);
+            AlphaAnimation animation = new AlphaAnimation(1, 0);
+            animation.setDuration(500);
+            animation.setFillAfter(true);
+            set.addAnimation(animation);
+            if (mThumbnailView != null) {
+                mThumbnailView.setAnimation(set);
+            }
+
+            if (mProgressIndicator != null) {
+                mProgressIndicator.onFinish();
+            }
+
+            if (mProgressIndicatorView != null) {
+                mProgressIndicatorView.setAnimation(set);
+            }
+
+            animation.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    if (mThumbnailView != null) {
+                        mThumbnailView.setVisibility(GONE);
+                    }
+                    if (mProgressIndicatorView != null) {
+                        mProgressIndicatorView.setVisibility(GONE);
+                    }
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+                }
+            });
+        } else {
+            if (mProgressIndicator != null) {
+                mProgressIndicator.onFinish();
+            }
+            if (mThumbnailView != null) {
+                mThumbnailView.setVisibility(GONE);
+            }
+            if (mProgressIndicatorView != null) {
+                mProgressIndicatorView.setVisibility(GONE);
+            }
+        }
     }
 
     @UiThread
