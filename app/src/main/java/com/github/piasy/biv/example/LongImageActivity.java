067f8eb86ee522190ab473f36a1b0de11c21a663
Piasy <xz4215@gmail.com>
2022-12-12 08:31:18 +0800

    update deps

--- app/src/main/java/com/github/piasy/biv/example/LongImageActivity.java
+++ app/src/main/java/com/github/piasy/biv/example/LongImageActivity.java
@@ -151,23 +151,24 @@ public class LongImageActivity extends AppCompatActivity {
 
     @SuppressWarnings("MissingPermission")
     private void saveImage() {
-        disposePermissionRequest();
-        mPermissionRequest = new RxPermissions(this)
-                .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                .observeOn(Schedulers.io())
-                .subscribe(new Consumer<Boolean>() {
-                    @Override
-                    public void accept(Boolean granted) throws Exception {
-                        if (granted) {
-                            mBigImageView.saveImageIntoGallery();
-                        }
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Exception {
-                        throwable.printStackTrace();
-                    }
-                });
+        mBigImageView.saveImageIntoGallery();
+        //disposePermissionRequest();
+        //mPermissionRequest = new RxPermissions(this)
+        //        .request(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        //        .observeOn(Schedulers.io())
+        //        .subscribe(new Consumer<Boolean>() {
+        //            @Override
+        //            public void accept(Boolean granted) throws Exception {
+        //                if (granted) {
+        //                    mBigImageView.saveImageIntoGallery();
+        //                }
+        //            }
+        //        }, new Consumer<Throwable>() {
+        //            @Override
+        //            public void accept(Throwable throwable) throws Exception {
+        //                throwable.printStackTrace();
+        //            }
+        //        });
     }
 
     private AlertDialog showDialog() {
